becaeefcc0dd2c4b7c900a0d0141cb80b29067e9
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-06 08:22:20 +0900

    Update Cuboid.java

--- src/Cuboid.java
+++ src/Cuboid.java
@@ -1,4 +1,19 @@
 package PACKAGE_NAME;
 
-public class Cuboid {
+public class Cuboid extends Rectangle{
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);   //Calling the parent constructor
+        this.height = (height < 0 ? 0: height);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return  getArea() * this.height;
+    }
 }
